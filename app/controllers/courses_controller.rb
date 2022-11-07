45545911b3fac77e53f7dc9d597da73fd263f7d6
Gustave <guymoustapha@gmail.com>
2022-11-07 13:43:58 +0200

    Activate ransack in the navbar

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,8 +7,10 @@ class CoursesController < ApplicationController
       @courses = Course.where('title ILIKE ?', "%#{params[:title]}%")
     else
       # @courses = Course.all
-      @q = Course.ransack(params[:q])
-      @courses = @q.result(distinct: true)
+      # @q = Course.ransack(params[:q])
+      # @courses = @q.result.includes(users)
+      @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+      @courses = @ransack_courses.result.includes(:user)
     end
   end
 
